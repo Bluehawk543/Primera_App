09b99d9bcd4c58402acd7144af01b2d666fef2e8
Bluehawk543 <143103576+Bluehawk543@users.noreply.github.com>
2023-08-24 10:39:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,24 +21,38 @@ col1, col2 = st.columns(2)
 with col1:
   st.subheader("Esta es la primera columna")
   st.write("Las interfaces multimodales mejoran la experiencia de usuario")
-  resp = st.checkbox("Estoy de acuerdo")
+  resp = st.checkbox("Estoy de acuerdo") # Caja de check
   if resp:
     st.write("¡Correcto!")
     
 with col2:
   st.subheader("Esta es la segunda columna")
   modo = st.radio("Que modalidad es la principal en tu interfaz",("Visual","Auditiva","Táctil"))
-  if modo == "Visual":
+  if modo == "Visual": # Selección Multiple
     st.write("La vista es fundamental para tu interfaz")
   if modo == "Auditiva":
     st.write("La audición es fundamental para tu interfaz")
   if modo == "Táctil":
     st.write("El tacto es fundamental para tu interfaz")
 
-# Botones
+# BOTONES
 st.subheader("Uso de botones")
 if st.button("Presiona el boton"):
   st.write("Gracias por presionar, sapa")
 else:
   st.write("P R E S I O N A M E... te reto")
+
+# MENU DESPLEGABLE
+st.subheader("Selectbox")
+in_mond = st.selectbox(
+  "Selecciona la modalidad",("Audio","Visual","Háptico")
+  )
+if in_mod == "Audio":
+  set_mod = "Reproducir audio"
+elif in_mod == "Visual":
+  set_mod = "Reproducir video"
+elif in_mod == "Háptico":
+  set_mod = "Reproducir vibración"
+st.write("La acción es:", set_mod)
+
 
