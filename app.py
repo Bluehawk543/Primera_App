b3cd406fca207eaf6c336e9a489a013ed320b026
Bluehawk543 <143103576+Bluehawk543@users.noreply.github.com>
2023-08-24 10:46:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,10 @@ elif in_mod == "Háptico":
   set_mod = "Reproducir vibración"
 st.write("La acción es:", set_mod)
 
+# BARRA LATERAL
+st.subheader("Configura la modalidad")
+mod_radio = st.radio(
+  "Escoge la modalidad a usar",
+  ("Visual","Auditiva","Háptica")
+)
 
